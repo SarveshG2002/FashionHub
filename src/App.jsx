283d1b9ac30ccb9a570364a84fede9e0f36774b5
Sarvesh Gandhere <sarveshgandhere2002@gmail.com>
2024-07-24 23:27:38 +0530

    AdminComponent Created

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Login from './Login.jsx';
+import AdminComponent from "./components/AdminComponent.jsx"
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <div className="App">
         <Routes>
           <Route path="/" element={<Login />}/>
+          <Route path="/admin" element={<AdminComponent />}/>
         </Routes>
       </div>
     </Router>
