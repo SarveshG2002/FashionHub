295e81a2b6052e31067128ef8bf534ffdb0a6ee1
Sarvesh Gandhere <sarveshgandhere2002@gmail.com>
2024-07-25 23:44:23 +0530

    Sidebar Updated

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+// import companyImage from "../../public/companylogo.png";
 // import '../assets/css/sidebar.css'; // You'll need to create this CSS file
 
 const Sidebar = () => {
@@ -10,6 +11,22 @@ const Sidebar = () => {
 
   return (
     <aside>
+      <div className='CompanyProfile'>
+        <div style={{height:"40px"}}>
+          <img src="/companylogo.png" style={{width:"100%",height:"100%",borderRadius:"50%"}}></img>
+        </div>
+        <div style={{fontSize:"20px",display:"flex",paddingLeft:"10px"}}>
+          FashionHub
+        </div>
+      </div>
+      <div className='CompanyProfile'>
+        <div style={{height:"40px"}}>
+          <img src="/companylogo.png" style={{width:"100%",height:"100%",borderRadius:"50%"}}></img>
+        </div>
+        <div style={{fontSize:"20px",display:"flex",paddingLeft:"10px"}}>
+          FashionHub
+        </div>
+      </div>
     </aside>
   );
 };
