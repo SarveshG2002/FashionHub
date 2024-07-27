c16fd7e1fce29eea56b006de2cb15ebe3894aa21
Sarvesh Gandhere <sarveshgandhere2002@gmail.com>
2024-07-27 22:41:49 +0530

    Brand List Table created

--- src/pages/BrandPage.jsx
+++ src/pages/BrandPage.jsx
@@ -1,23 +1,23 @@
 import React from 'react';
 import axios from 'axios';
 import { BASE_URL } from '../components/Host.jsx';
-import { useEffect,useState } from 'react';
+import { useEffect, useState } from 'react';
 
 function Component() {
 
-    const pageName = useState("Brand");
-    const [brandName,setBrandName] = useState("");
-    const [brands,setBrands] = useState([]);
-    useEffect(()=>{
+    const [pageName] = useState("Brand");
+    const [brandName, setBrandName] = useState("");
+    const [brands, setBrands] = useState([]);
+    useEffect(() => {
         getAllbrands();
-    },[])
+    }, [])
 
-    const getAllbrands = async ()=>{
-        try{
+    const getAllbrands = async () => {
+        try {
             let brands = await axios.post(`${BASE_URL}/brands/getAllBrands`);
             console.log(brands);
             setBrands(brands.data.data);
-        }catch(error){
+        } catch (error) {
             console.log(error);
         }
     }
@@ -32,7 +32,7 @@ function Component() {
 
             console.log(response.data);
 
-            
+
         } catch (error) {
             console.error("Error during login:", error);
             // setError('An error occurred. Please try again.');
@@ -40,27 +40,67 @@ function Component() {
     };
 
     return (
-        <div className="card">
-            <div className="card-header">
-                Add {pageName}
+        <>
+            <div className="card">
+                <div className="card-header">
+                    Add {pageName}
+                </div>
+                <div className="card-body">
+                    <form className="col-md-12 row" encType="multipart/form-data" onSubmit={handleSubmit}>
+                        <div className="col-md-12 row">
+                            <div className="col-md-6 form-group">
+                                <label htmlFor="category_name">{pageName} Name</label>
+                                <input type="text" className="form-control" id="brand_name" name="brand_name" placeholder="Enter Here" onChange={(e) => setBrandName(e.target.value)} />
+                            </div>
+                            <div className="col-md-6 form-group">
+                                {/* You can add more form fields here if needed */}
+                            </div>
+                            <div className="col-md-6 mt-3 form-group">
+                                <input type="submit" name="submit" value="Add" className="btn btn-primary" />
+                            </div>
+                        </div>
+                    </form>
+                </div>
             </div>
-            <div className="card-body">
-                <form className="col-md-12 row" encType="multipart/form-data" onSubmit={handleSubmit}>
-                    <div className="col-md-12 row">
-                        <div className="col-md-6 form-group">
-                            <label htmlFor="category_name">{pageName} Name</label>
-                            <input type="text" className="form-control" id="brand_name" name="brand_name" placeholder="Enter Here" onChange={(e)=>setBrandName(e.target.value)}/>
-                        </div>
-                        <div className="col-md-6 form-group">
-                            {/* You can add more form fields here if needed */}
-                        </div>
-                        <div className="col-md-6 mt-3 form-group">
-                            <input type="submit" name="submit" value="Add" className="btn btn-primary" />
-                        </div>
-                    </div>
-                </form>
+            <div className="card">
+                <div className="card-header">
+                    Brand List
+                </div>
+                <div className="card-body table-responsive">
+                    <table className="table table-striped table-hover">
+                        <thead>
+                            <tr>
+                                <th>
+                                    Sr.
+                                </th>
+                                <th>
+                                    Brand Name
+                                </th>
+                                <th>
+                                    Action
+                                </th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                        {brands.map((brand, key) => (
+                            <tr key={key}>
+                                <td>{key + 1}</td>
+                                <td>{brand.brand_name}</td>
+                                <td>
+                                    <button className="btn btn-primary">
+                                        Edit
+                                    </button>
+                                    <button className='btn btn-danger'>
+                                        Delete
+                                    </button>
+                                </td>
+                            </tr>
+                        ))}
+                        </tbody>
+                    </table>
+                </div>
             </div>
-        </div>
+        </>
     );
 }
 
