8e6c58c2831961c46b39b22f121e0ff9757ab2b6
Sarvesh Gandhere <sarveshgandhere2002@gmail.com>
2024-08-06 22:17:18 +0530

    Modal Adde for editing purpose

--- src/pages/VariantsPage.jsx
+++ src/pages/VariantsPage.jsx
@@ -23,6 +23,8 @@ function Component() {
     const [extraLargeSizeQuantity, setExtraLargeSizeQuantity] = useState("0");
     const [extraExtraLargeSizeQuantity, setExtraExtraLargeSizeQuantity] = useState("0");
 
+    const [showModal, setShowModal] = useState(true);
+
     useEffect(() => {
         initiate();
     }, []);
@@ -72,6 +74,261 @@ function Component() {
     }
     return (
         <>
+
+            {showModal && (
+                <div className="modal fade show" style={{ display: 'block' }} tabIndex="-1" role="dialog">
+                    <div className="modal-dialog modal-lg" role="document">
+                        <div className="modal-content">
+                            <div className="modal-header">
+                                <h5 className="modal-title">Edit {pageName}</h5>
+                                <button type="button" className="close">
+                                    <span>&times;</span>
+                                </button>
+                            </div>
+                            <div className="modal-body">
+                            <form className="col-md-12 row" encType="multipart/form-data" onSubmit={handleSubmit}>
+                        <div className="col-md-12 row">
+                            <div className="col-md-6 form-group">
+                                <label htmlFor="varient_name">{pageName} Name</label>
+                                <input
+                                    type="text"
+                                    className="form-control"
+                                    id="varient_name"
+                                    name="varient_name"
+                                    placeholder="Enter Here"
+                                    value={categoryName}
+                                    onChange={(e) => setCategoryName(e.target.value)}
+                                />
+                            </div>
+
+                            <div className="col-md-6 form-group">
+                                <label htmlFor="product">Product</label>
+                                <select
+                                    id="product"
+                                    name="product"
+                                    className='form-control'
+                                    value={productid}
+                                    onChange={(e) => setProductId(e.target.value)}
+                                >
+                                    <option value="">Select Product</option>
+                                    {productList.map((product) => (
+                                        <option key={product._id} value={product._id}>
+                                            {product.product_name}
+                                        </option>
+                                    ))}
+                                </select>
+                            </div>
+
+                            <div className="col-md-6 form-group">
+                                <label htmlFor="regular_price">Regular Price</label>
+                                <input
+                                    type="text"
+                                    className="form-control"
+                                    id="regular_price"
+                                    name="regular_price"
+                                    placeholder="Enter Here"
+                                    value={regularPrice}
+                                    onChange={(e) => setRegularPrice(e.target.value)}
+                                />
+                            </div>
+
+                            <div className="col-md-6 form-group">
+                                <label htmlFor="selling_price">Selling Price</label>
+                                <input
+                                    type="text"
+                                    className="form-control"
+                                    id="selling_price"
+                                    name="selling_price"
+                                    placeholder="Enter Here"
+                                    value={sellingPrice}
+                                    onChange={(e) => setSellingPrice(e.target.value)}
+                                />
+                            </div>
+
+                            <div className="col-md-6 form-group">
+                                <label htmlFor="color">Color</label><br />
+                                <input
+                                    type="color"
+                                    className=""
+                                    id="color"
+                                    name="color"
+                                    placeholder="Enter Here"
+                                    value={color}
+                                    onChange={(e) => setColor(e.target.value)}
+                                />
+                            </div>
+                            <div className="col-md-6 form-group">
+                                <label htmlFor="height">Height</label>
+                                <input
+                                    type="text"
+                                    className="form-control"
+                                    id="height"
+                                    name="height"
+                                    placeholder="Enter Here"
+                                    value={height}
+                                    onChange={(e) => setHeight(e.target.value)}
+                                />
+                            </div>
+                            <div className="col-md-6 form-group">
+                                <label htmlFor="width">Width</label>
+                                <input
+                                    type="text"
+                                    className="form-control"
+                                    id="width"
+                                    name="width"
+                                    placeholder="Enter Here"
+                                    value={width}
+                                    onChange={(e) => setWidth(e.target.value)}
+                                />
+                            </div>
+                            <div className="col-md-6 form-group">
+                                <label htmlFor="bredth">Bredth</label>
+                                <input
+                                    type="text"
+                                    className="form-control"
+                                    id="bredth"
+                                    name="bredth"
+                                    placeholder="Enter Here"
+                                    value={bredth}
+                                    onChange={(e) => setBredth(e.target.value)}
+                                />
+                            </div>
+                            <div className="col-md-6 form-group">
+                                <label htmlFor="weigth">Weight</label>
+                                <input
+                                    type="text"
+                                    className="form-control"
+                                    id="weigth"
+                                    name="weigth"
+                                    placeholder="Enter Here"
+                                    value={weight}
+                                    onChange={(e) => setWeight(e.target.value)}
+                                />
+                            </div>
+
+                            <div className="col-md-6 form-group">
+                                <label htmlFor="varient_desc">{pageName} Description</label>
+                                <textarea
+                                    className="form-control"
+                                    id="varient_desc"
+                                    name="varient_desc"
+                                    placeholder="Enter Here"
+                                    value={varientDesc}
+                                    onChange={(e) => setVarientDesc(e.target.value)}
+                                />
+                            </div>
+
+                            <div className="card-body table-responsive">
+                                <table className="table table-striped table-hover">
+                                    <thead>
+                                        <tr>
+                                            <th>
+                                                {pageName} Size
+                                            </th>
+                                            <th>
+                                                Quantity
+                                            </th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        <tr>
+                                            <td>
+                                                Small (S)
+                                            </td>
+                                            <td>
+                                                <input
+                                                    type="text"
+                                                    className="form-control"
+                                                    id="smallSizeQuantity"
+                                                    name="smallSizeQuantity"
+                                                    placeholder="Enter Here"
+                                                    value={smallSizeQuantity}
+                                                    onChange={(e) => setSmallSizeQuantity(e.target.value)}
+                                                />
+                                            </td>
+                                        </tr>
+                                        <tr>
+                                            <td>
+                                                Medium (M)
+                                            </td>
+                                            <td>
+                                                <input
+                                                    type="text"
+                                                    className="form-control"
+                                                    id="mediumSizeQuantity"
+                                                    name="mediumSizeQuantity"
+                                                    placeholder="Enter Here"
+                                                    value={mediumSizeQuantity}
+                                                    onChange={(e) => setMediumSizeQuantity(e.target.value)}
+                                                />
+                                            </td>
+                                        </tr>
+                                        <tr>
+                                            <td>
+                                                Large (L)
+                                            </td>
+                                            <td>
+                                                <input
+                                                    type="text"
+                                                    className="form-control"
+                                                    id="largeSizeQuantity"
+                                                    name="largeSizeQuantity"
+                                                    placeholder="Enter Here"
+                                                    value={largeSizeQuantity}
+                                                    onChange={(e) => setLargeSizeQuantity(e.target.value)}
+                                                />
+                                            </td>
+                                        </tr>
+                                        <tr>
+                                            <td>
+                                                Extra Large (XL)
+                                            </td>
+                                            <td>
+                                                <input
+                                                    type="text"
+                                                    className="form-control"
+                                                    id="extraLargeSizeQuantity"
+                                                    name="extraLargeSizeQuantity"
+                                                    placeholder="Enter Here"
+                                                    value={extraLargeSizeQuantity}
+                                                    onChange={(e) => setExtraLargeSizeQuantity(e.target.value)}
+                                                />
+                                            </td>
+                                        </tr>
+                                        <tr>
+                                            <td>
+                                                Extra Extra Large (XXL)
+                                            </td>
+                                            <td>
+                                                <input
+                                                    type="text"
+                                                    className="form-control"
+                                                    id="extraExtraLargeSizeQuantity"
+                                                    name="extraExtraLargeSizeQuantity"
+                                                    placeholder="Enter Here"
+                                                    value={extraExtraLargeSizeQuantity}
+                                                    onChange={(e) => setExtraExtraLargeSizeQuantity(e.target.value)}
+                                                />
+                                            </td>
+                                        </tr>
+                                    </tbody>
+                                </table>
+                            </div>
+
+                            <div className="col-md-12 mt-3 form-group">
+                                <input type="submit" name="submit" value="Add" className="btn btn-primary" />
+                            </div>
+                        </div>
+                    </form>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            )}
+
+            {showModal && <div className="modal-backdrop fade show"></div>}
+
+
             <div className="card">
                 <div className="card-header">
                     Add {pageName}
