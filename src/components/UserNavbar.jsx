30256dece0d7fc1e771539b49547b2a7d5e1fd21
Sarvesh Gandhere <sarveshgandhere2002@gmail.com>
2024-08-23 23:16:48 +0530

    Tailwind configured successfully

--- src/components/UserNavbar.jsx
+++ src/components/UserNavbar.jsx
@@ -4,7 +4,9 @@ import React from 'react';
 function Component() {
     return (
         <div>
-
+            <div className="h-screen flex items-center justify-center bg-blue-500">
+      <h1 className="text-4xl text-white font-bold">Welcome to Tailwind in React!</h1>
+    </div>
         </div>
     );
 }
