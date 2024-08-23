21a729ccfdaf28100b39954a7707987eb51eb368
Sarvesh Gandhere <sarveshgandhere2002@gmail.com>
2024-08-23 23:33:25 +0530

    Navbar working started

--- src/components/UserNavbar.jsx
+++ src/components/UserNavbar.jsx
@@ -1,14 +1,23 @@
 import React from 'react';
-// import '../assets/css/';
 
-function Component() {
+function Navbar() {
     return (
-        <div>
-            <div className="h-screen flex items-center justify-center bg-blue-500">
-      <h1 className="text-4xl text-white font-bold">Welcome to Tailwind in React!</h1>
-    </div>
+        <div className="w-full p-4 bg-gray-800">
+            <div className="container mx-auto flex items-center justify-between">
+                {/* Logo */}
+                <div className="text-white font-bold text-xl">
+                    <img src="logo.png" alt="Logo" className="h-10" />
+                </div>
+
+                {/* Other Navbar Items (e.g., Links) */}
+                <div className="flex space-x-4">
+                    <a href="/" className="text-white hover:text-gray-400">Home</a>
+                    <a href="/about" className="text-white hover:text-gray-400">About</a>
+                    <a href="/contact" className="text-white hover:text-gray-400">Contact</a>
+                </div>
+            </div>
         </div>
     );
 }
 
-export default Component;
+export default Navbar;
