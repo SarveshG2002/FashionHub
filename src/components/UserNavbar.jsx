bb7eba82aafd364b62ca393720ba858c975a7bfb
Sarvesh Gandhere <sarveshgandhere2002@gmail.com>
2024-09-13 21:30:03 +0530

    Navbar updated

--- src/components/UserNavbar.jsx
+++ src/components/UserNavbar.jsx
@@ -1,18 +1,30 @@
 import React from 'react';
 import "../assets/css/navbar.css";
+import { FaUser } from "react-icons/fa";
+
 
 function Navbar() {
     return (
-        <div className="navbar">
+        <div className="navbar1">
             
                 <div className="nav-image">
                     <img src="logo.png" alt="Logo" className="" />
                 </div>
 
-                <div className="nav-navigation">
-                    <a href="/" className="">Home</a>
-                    <a href="/about" className="">About</a>
-                    <a href="/contact" className="">Contact</a>
+                <div className='nav-navigation'>
+                    <span>HOME</span>
+                    <span>SHOP</span>
+                    <span>CATEGORIES</span>
+                    <span>BRANDS</span>
+                    <span>ABOUT US</span>
+                </div>
+
+                <div className='search'>
+                    <input className='input' placeholder='Search'></input>
+                </div>
+
+                <div className='user'>
+                    <FaUser />
                 </div>
            
         </div>
